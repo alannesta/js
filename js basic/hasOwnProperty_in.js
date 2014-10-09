ef849aee77695d14e7d98875bc85aa7b0ca8c825
alan cao <acao@moneycloud.me>
2014-10-09 12:17:41 -0400

    has own property does not iterate through prototypes

---
+++ js basic/hasOwnProperty_in.js
@@ -0,0 +1,44 @@
+/*
+	Every object descended from Object inherits the hasOwnProperty method. This method can be used to determine whether an object has the specified property as a direct property of that object.
+
+	unlike the in operator, this method does not check down the object's prototype chain!
+*/
+
+function Person(name, age){
+	this.name = name,
+	this.age = age,
+	this.sayName = function(){
+		console.log(this.name);
+	}
+	this.sayAge = function(){
+		console.log("age: " + this.age);
+	}
+}
+
+Person.prototype.getAge = function(){
+	console.log(this.age);
+}
+
+function Coder(name, age){
+	this.name = name,
+	this.age = age,
+	this.getInfo = function(){
+		console.log("name: "+name+", age: "+age);
+	}
+}
+	
+Coder.prototype = new Person();		//the key to prototype inheritance---> point the child's prototype to parent's
+
+var coder = new Coder("Markelov", 100);
+coder.sayName();	//this is also inherited?
+coder.sayAge();		//how is this inherited?
+
+
+for (var prop in coder) {
+  if (coder.hasOwnProperty(prop)) {
+    console.log("Has own property: " + prop);
+  }
+  else {
+    console.log("in: " + prop); // toString or something else
+  }
+}
