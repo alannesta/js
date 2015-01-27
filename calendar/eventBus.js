7da55cdd1e2673c44850c582dad34f2e1386aa70
alan cao <alannesta@sina.com>
2015-01-27 13:13:12 -0500

    event bus

---
+++ calendar/eventBus.js
@@ -0,0 +1,59 @@
+var EventBus = function(){
+	this.handlers = {};
+}	
+
+EventBus.prototype.on = function(event, handler){
+	if(this.handlers[event]){
+		// Check duplication?
+		this.handlers[event].push(handler);
+	}else{
+		this.handlers[event] = [];
+		this.handlers[event].push(handler);
+	}
+}
+
+EventBus.prototype.trigger = function(event){
+	// debugger;
+	if (this.handlers[event]){
+		for (var i=0, length = this.handlers[event].length; i<length; i++ ){
+			this.handlers[event][i]();
+		}
+	}
+}	
+
+EventBus.prototype.off = function(event, handler){
+	if (this.handlers[event] instanceof Array){
+		for (var i=0, length = this.handlers[event].length; i<length; i++){
+			if (this.handlers[event][i] === handler){
+				break;
+			}
+		}
+		this.handlers[event].splice(i,1);
+	}
+}
+
+
+// inheritance demo
+
+var CalendarWidget = function(){
+	var self = this;
+	// EventBus.apply(self);	// inherit instance variables, in this case: this.handlers
+}
+CalendarWidget.prototype = new EventBus();	// inherit prototype method
+
+
+var cal1 = new CalendarWidget();
+var cal2 = new CalendarWidget();
+
+cal1.on('date change', function(){
+	console.log('cal1 date change');
+});
+
+cal2.on('date change', function(){
+	console.log('cal2 date change');
+});
+
+console.log(cal1);
+console.log(cal2);
+// cal1.trigger('date change');
+// cal2.trigger('date change');
