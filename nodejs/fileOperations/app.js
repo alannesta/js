02e57ab60e9632cb61087e5cee8604f5cd0c4483
Alan Cao <alannesta@sina.com>
2015-02-09 08:03:54 -0500

    file operations

--- nodejs/fileOperations/app.js
+++ nodejs/fileOperations/app.js
@@ -1,26 +1,46 @@
 var fs = require('fs');
 var folder = 'data';
-
+var model = {'data': []};
 
 function readFile(path) {
+
+    // asynchronizely readFile 
     fs.readFile(path, 'utf-8', function (err, data) {
         if (err) throw err;
         console.log(data);
     });
+
+    // synchronizely readFile, no callback
+    // try{
+    //     console.log(fs.readFileSync(path, 'utf-8')); 
+    // }catch (err){
+    //     console.log(err);
+    // }
 }
 
-function readDirectory(folder){
+function readDirectory(folder, depth){
     fs.readdir(folder, function (err, files) {
         if (err) throw err;
         files.forEach(function (item) {
             var filePath = folder + '/' + item;
+            var temp = model;
             if (isFile(filePath) && filterFile(filePath)) {
-                console.log('Path: '+filePath);
-                readFile(filePath);
+                
+                for (var i = 0; i<depth.length;i++){
+                    temp = temp[depth[i]];
+                }
+                temp.push(item);
+                // readFile(filePath);
             }else if(isDirectory(filePath)){
-                console.log(filePath);
-                readDirectory(filePath);
+                for (var i = 0; i<depth.length;i++){
+                    temp = temp[depth[i]];
+                }
+                temp.push({item: []});
+                // console.log(filePath);
+                var dep = filePath.split('/');
+                readDirectory(filePath, dep);
             }
+
         });
     });
 }
@@ -43,4 +63,7 @@ function filterFile(filePath){
 
 }
 
-readDirectory(folder);
+readDirectory(folder, [folder]);
+setTimeout(function(){
+    console.log(model);
+},1000);
