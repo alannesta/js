29c04823f98116e97a5e6a5fd23a62edd94c8db3
sijin cao <scao@acquisio.com>
2015-05-06 16:16:04 -0400

    add some use cases

--- probability/util.js
+++ probability/util.js
@@ -20,4 +20,10 @@ function combination(n, k) {
 }
 
 // console.log(factorial(0));
-// console.log(combination(27, 22)/combination(30, 25));
+console.log((combination(26, 7)*4 + combination(26, 6)*2 + combination(26,5) ) / combination(30, 9));	// 9 fei paoxiao yi tao
+console.log((combination(26, 9)*4 + combination(26, 8)*2 + combination(26,7) ) / combination(30, 11));	// 9 fei paoxiao yi tao (guo 2 zhang pai)
+
+console.log(1- (combination(28, 9)/combination(30, 9)));	// 6 fei you shi zi, bai fu mei
+console.log(1- (combination(28, 10)/combination(30, 10)));	// 6 fei you shi zi, bai fu mei (guo 1 zhang pai or hou shou)
+
+console.log(1- (combination(28, 10)/combination(30, 10)));	// 7 fei you peng peng
