b35f201ad05dd4203636e162969ee9687cdddc73
sijin cao <scao@acquisio.com>
2015-05-19 16:47:22 -0400

    bind the callback in forEach iterator

--- nodejs/async/app.js
+++ nodejs/async/app.js
@@ -1,12 +1,12 @@
 var async = require('async');
 var arr = [1, 2, 3, 4, 5, 6];
 
-arr.forEach(function(item) {
-    setTimeout(function() {
-        console.log(item);
-    });
-});
-console.log('done');    // 'done' will be logged before all the items
+//arr.forEach(function(item) {
+//    setTimeout(function() {
+//        console.log(item);
+//    });
+//});
+//console.log('done');    // 'done' will be logged before all the items
 
 
 /*
@@ -14,11 +14,18 @@ console.log('done');    // 'done' will be logged before all the items
  *
  */
 
-//async.each(arr, function(item, callback){
-//    setTimeout(function(){
-//        console.log(item);
-//        callback();
-//    },500);
-//}, function(){
-//    console.log('done');      // 'done' is logged at after all the items have been logged
-//});
+async.each(arr, iterator.bind(this, cb), function(){
+    console.log('done');      // 'done' is logged at after all the items have been logged
+});
+
+function iterator() {
+    var args = Array.prototype.slice.call(arguments);
+    setTimeout(function(){
+        console.log(args[1]);   // item looped by forEach is now args 1...
+        args[0]();      // the cb which is bound...
+    },500);
+}
+
+function cb() {
+    console.log('callback');
+}
