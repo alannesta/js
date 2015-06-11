59be7d63159faac9e14eeb2b9975ac79426fbc32
Alan Cao <alannesta@sina.com>
2015-06-11 10:20:44 -0400

    browserify and gulp task

---
+++ nodejs/browserify/app/getOwnPropertyNames.js
@@ -0,0 +1,91 @@
+/*
+* Note that the for...in loop is already only iterating enumerable items,
+* so one should not assume based on the lack of non-enumerable properties shown in the loop that hasOwnProperty
+* itself is confined strictly to enumerable items (as with Object.getOwnPropertyNames()).
+*
+* */
+
+var _ = require('underscore');
+var chalk = require('chalk');
+
+function Rectangle(width, height) {
+    this.width = width;
+    this.height = height;
+    this.area = function() {
+        return this.height * this.width;
+    }
+}
+
+Rectangle.prototype.perimeter = function() {
+    return (this.width + this.height) * 2;
+};
+
+var rectangle = new Rectangle(100, 100);
+
+var square = Object.create(rectangle);
+//var square = Object.create(new Rectangle());      // all props on square's prototype
+//var square = Object.create(Rectangle.prototype);  // only 'perimeter' is inherited on square's prototype
+//var square = _.extend({}, rectangle);     // all props become square's 'own' property
+
+
+//var parent = {
+//    width: 100,
+//    height: 100,
+//    get area() {
+//        return this.width * this.height;
+//    }
+//};
+//
+//Object.defineProperty(parent, 'perimeter', {
+//   get: function() {
+//       return (this.width + this.height) * 2;
+//   }
+//});
+
+//parent.prototype.getPerimeter = function() {
+//    return (this.width + this.height) * 2;
+//};
+
+function getProperties(obj) {
+    console.log(chalk.blue('-------- List property ---------------------'));
+    console.log('Object.keys() ---> ', Object.keys(obj));
+    console.log('getOwnPropertyNames() ---> ', Object.getOwnPropertyNames(obj));
+}
+
+
+
+/**
+ *
+ * @param {object} obj
+ * @description
+ * demonstrate that "for...in..." loop will go up alone the prototype chain
+ */
+function forInLoopDescriptorAndProperty(obj) {
+    console.log(chalk.blue('---------for in loop -----------'));
+    for (var key in obj ) {
+        if (obj.hasOwnProperty(key)) {
+            console.log('hasOwnProperty: ---> ' + chalk.red(key));
+        }else{
+            console.log('enumerable props from prototype chain ---> ' + chalk.red(key));
+        }
+        console.log('getOwnPropertyDescriptor ---> ' + chalk.red(key), Object.getOwnPropertyDescriptor(obj, key));
+    }
+}
+
+function getOwnPropertyNamesDescriptorAndProperty(obj) {
+    console.log('--------- get own property names -----------');
+    Object.getOwnPropertyNames(obj).forEach(function(key) {
+        if (obj.hasOwnProperty(key)) {
+            console.log('hasOwnProperty: ---> ' + chalk.red(key));
+        }else{
+            console.log('enumerable props from prototype chain ---> ' + chalk.red(key));
+        }
+        console.log('getOwnPropertyDescriptor ---> ' + chalk.red(key), Object.getOwnPropertyDescriptor(obj, key));
+    });
+}
+
+//forInLoopDescriptorAndProperty(rectangle);
+forInLoopDescriptorAndProperty(square);
+getProperties(rectangle);
+getOwnPropertyNamesDescriptorAndProperty(square);
+
--- nodejs/browserify/gulpfile.js
+++ nodejs/browserify/gulpfile.js
@@ -2,20 +2,30 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var inject = require('gulp-inject');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('default', ['inject'], function() {
-	return browserify('./app/demo.js')
-	.bundle()
-    //Pass desired output filename to vinyl-source-stream
-    .pipe(source('bundle.js'))
-    // Start piping stream to tasks!
-    .pipe(gulp.dest('./dist/'));
-
+	console.log('all done');
 });
 
-gulp.task('inject', function(){
-	var jsfile = gulp.src('./dist/*.js');
-	var target = gulp.src('./demo.html');
-	return target.pipe(inject(gulp.src('./dist/*.js')), {addRootSlash: false}).
-			pipe(gulp.dest('./'));
+gulp.task('build', ['concat'], function() {
+	return browserify('./tmp/all.js')
+		.bundle()
+		//Pass desired output filename to vinyl-source-stream
+		.pipe(source('bundle.js'))
+		// Start piping stream to tasks!
+		.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('concat', function() {
+	return gulp.src('./app/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('./tmp'))
+});
+
+
+gulp.task('inject', ['build'], function(){
+	return gulp.src('./app/app.html')
+		.pipe(inject(gulp.src('./dist/*.js')), {read:false, relative:false})
+		.pipe(gulp.dest('./dist'));
 })
