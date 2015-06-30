ef45be4dcce11cd42c9d2331a8eef7b10fb4d50c
Alan Cao <alannesta@sina.com>
2015-06-30 10:23:51 -0400

    livereload proof of concept

--- nodejs/browser-reload/server.js
+++ nodejs/browser-reload/server.js
@@ -3,18 +3,27 @@ var express = require('express');
 var http = require('http').Server(app);
 var Server = require('socket.io');
 
-var io = new Server(31203);
-
-//app.get('/', function(req, res){
-//    res.sendFile(__dirname + '/static/app.html');
-//});
+var io = new Server(31203);     // create the socket.io(websocket) server on a custom port
 
 app.use(express.static('static'));
 
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/static/app.html');
+});
+
+app.get('/reload', function() {
+    io.emit('reload allowed');
+});
+
 io.on('connection', function(socket){
-    console.log('new connection');
+    //console.log('new connection: ',socket);
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
+    });
+
+    socket.on('reload request', function() {
+       io.emit('reload allowed');
     });
 });
 
