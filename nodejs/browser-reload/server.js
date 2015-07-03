bc8c68a8790cf9e9e2c192e73d5b86b81f3a44e1
Alan Cao <alannesta@sina.com>
2015-07-03 11:05:19 -0400

    reorganize the code and update client upon connection

--- nodejs/browser-reload/server.js
+++ nodejs/browser-reload/server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var http = require('http').Server(app);
 var Server = require('socket.io');
 
+var currentMessage = '';
+
 var io = new Server(31203);     // create the socket.io(websocket) server on a custom port
 
 app.use(express.static('static'));
@@ -38,37 +40,50 @@ io.on('connection', function(socket){
 /*
 * namespacing connections
 * */
-//var board = io.of('/board').on('connection', function(socket) {
-//    // assign the unique id value
-//    socket.emit('server:assign', socket.id);    // only emit event to the socket itself
-//    // what is the socket here?
-//    socket.on('client:typing', function(data) {
-//        // what are the differences?
-//        board.emit('server:update', data.msg);        // standard way, to all sockets under '/board'
-//        //socket.broadcast.emit('server:update', data.msg);     // this works
-//        //io.sockets.emit('server:update', data.msg);     // does not work, not properly namespaced
-//        //socket.emit('server:update', data.msg);       // this does not work
-//
-//        console.log(data.clientID + ' is typing ');
-//    })
-//});
+var board = io.of('/board').on('connection', function(socket) {
+    updateClient(socket);
+    // assign the unique id value
+    socket.emit('server:assign', socket.id);    // only emit event to the socket itself
+    // what is the socket here?
+    socket.on('client:typing', function(data) {
+        currentMessage = data.msg;
+        updateClient(board);
+        // what are the differences?
+        //board.emit('server:update', data.msg);        // standard way, to all sockets under '/board'
+        //socket.broadcast.emit('server:update', data.msg);     // this works
+        //io.sockets.emit('server:update', data.msg);     // does not work, not properly namespaced
+        //socket.emit('server:update', data.msg);       // this does not work
+
+        console.log(data.clientID + ' is typing ');
+    })
+});
 
 /*
 * Using rooms
 *
 * */
-var board = io.of('/board').on('connection', function(socket) {
-    // assign the unique id value
-    socket.emit('server:assign', socket.id);    // only emit event to the socket itself
-    socket.join('sprint1');      // join a specific room
-    // what is the socket here?
-    socket.on('client:typing', function(data) {
+//var board = io.of('/board').on('connection', function(socket) {
+//    updateClient(socket);
+//    // assign the unique id value
+//    socket.emit('server:assign', socket.id);    // only emit event to the socket itself
+//    socket.join('sprint1');      // join a specific room
+//    // what is the socket here?
+//    socket.on('client:typing', function(data) {
+//        currentMessage = data.msg;
+//        //board.emit('server:update', data.msg);        // works, to all in namespace
+//        //socket.broadcast.emit('server:update', data.msg);   // works, to all in namespace
+//        //board.in('sprint1').emit('server:update', data.msg);        // only emit to a specific room
+//        updateClient(board.in('sprint1'));
+//    })
+//});
 
-        board.in('sprint1').emit('server:update', data.msg);        // only emit to a specific room
-        console.log(data.clientID + ' is typing ');
-    })
-});
-
+/**
+ *
+ * @param socket.io Namespace object
+ */
+function updateClient(sockets) {
+    sockets.emit('server:update', currentMessage);
+}
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
