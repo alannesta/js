6d111b3e95d8c76667b8f3c97825a6c3f24e601e
Alan Cao <alannesta@sina.com>
2015-07-05 12:19:27 -0400

    better type hint display

--- nodejs/browser-reload/static/scripts/board.js
+++ nodejs/browser-reload/static/scripts/board.js
@@ -1,6 +1,7 @@
 var socket = io('http://localhost:31203/board');    // name space the socket connection
 
 var clientID;
+var timeout;
 
 $('#dashboard').on('keyup', function () {
     socket.emit('client:typing', {
@@ -12,17 +13,21 @@ $('#dashboard').on('keyup', function () {
 });
 
 socket.on('server:update', function (data) {
+    clearTimeout(timeout);
     $('#dashboard').val(data.msg);
     if (data.user !== undefined) {
         $('#hint').show();
         $('#hint').text(data.user + ' is typing...');
     }
+    timeout = setTimeout(function() {
+        $('#hint').hide();
+    }, 1000);
 });
 
 socket.on('server:assign', function(id) {
     clientID = id;
 });
 
-setInterval(function() {
-    $('#hint').hide();
-}, 1000);
+//setInterval(function() {
+//    $('#hint').hide();
+//}, 1000);
