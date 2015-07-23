2d0e6ea285bdb6ecc2d76de22fc0f16cce13d48f
Alan Cao <alannesta@sina.com>
2015-07-22 23:02:32 -0400

    synchronous TCP connection in chrome script blocking

--- nodejs/quick_express/quick_server/routes/script-loading.js
+++ nodejs/quick_express/quick_server/routes/script-loading.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    //setTimeout(function() {
-    //    res.status(200).send('ok');
-    //}, 2000);
+
     res.render('script-loading');
 });
 
+router.get('/pending', function(req, res) {
+    setTimeout(function() {
+        res.status(200).send('ok');
+    }, 1000);
+});
 
 module.exports = router;
