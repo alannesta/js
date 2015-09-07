d77e02509f0188b242817afe5b517642903de98d
Alan Cao <alannesta@sina.com>
2015-09-06 20:20:58 -0400

    ES6 init

---
+++ ES6/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var _ = require('lodash');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var config = {
+  entryFile: './src/app.js',
+  outputDir: './dist/',
+  outputFile: 'app.js'
+};
+
+var bundler;
+function getBundler() {
+  if (!bundler) {
+    bundler = watchify(browserify(config.entryFile, _.extend({ debug: true }, watchify.args)));
+  }
+  return bundler;
+};
+
+function bundle() {
+  return getBundler()
+    .transform(babelify)
+    .bundle()
+    .on('error', function(err) { console.log('Error: ' + err.message); })
+    .pipe(source(config.outputFile))
+    .pipe(gulp.dest(config.outputDir))
+    .pipe(reload({ stream: true }));
+}
+
+gulp.task('build-persistent', function() {
+  return bundle();
+});
+
+gulp.task('build', ['build-persistent'], function() {
+  process.exit(0);
+});
+
+gulp.task('watch', ['build-persistent'], function() {
+
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  getBundler().on('update', function() {
+    gulp.start('build-persistent')
+  });
+});
+
+// WEB SERVER
+gulp.task('serve', function () {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+});
