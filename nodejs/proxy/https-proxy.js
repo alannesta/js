f29004e7d9a3b6c00625fe37ba2844247cefadb8
Alan Cao <alannesta@sina.com>
2016-02-25 13:40:27 -0500

    a few notes about streams

--- nodejs/proxy/https-proxy.js
+++ nodejs/proxy/https-proxy.js
@@ -5,30 +5,27 @@ var net = require('net');
 var url = require('url');
 
 function request(cReq, cRes) {
-	console.log('request');
-
 	var u = url.parse(cReq.url);
 
 	var options = {
-		hostname : u.hostname,
-		port     : u.port || 80,
-		path     : u.path,
-		method     : cReq.method,
-		headers     : cReq.headers
+		hostname: u.hostname,
+		port: u.port || 80,
+		path: u.path,
+		method: cReq.method,
+		headers: cReq.headers
 	};
 	var pReq = http.request(options, function(pRes) {
 		cRes.writeHead(pRes.statusCode, pRes.headers);
-		pRes.pipe(cRes);
+		pRes.pipe(cRes);	// pRes here in http.IncomingMessage rather than http.serverResponse, it is a readable stream
 	}).on('error', function(err) {
 		console.log(err);
 		cRes.end();
 	});
 
-	cReq.pipe(pReq);
+	cReq.pipe(pReq);	// cReq here is http.IncomingMessage rather than http.clientRequest, a readable stream not a writable stream
 }
 
 function connect(cReq, cSock) {
-	console.log('connect');
 	var u = url.parse('http://' + cReq.url);
 
 	var pSock = net.connect(u.port, u.hostname, function() {
@@ -45,7 +42,6 @@ var options = {
 	key: fs.readFileSync('./certificate/private.pem'),
 	cert: fs.readFileSync('./certificate/public.pem')
 };
-
 
 
 var server = https.createServer(options, request);
