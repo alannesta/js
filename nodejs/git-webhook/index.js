950743f4f50d2d54e99446eab240a77e7ff9057c
Alan Cao <alannesta@sina.com>
2016-03-17 14:48:54 -0400

    child process spawn

--- nodejs/git-webhook/index.js
+++ nodejs/git-webhook/index.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const path = require('path');
-//const exec = require('child_process').exec;
-const spawn = require('child_process').spawn;
+const bodyParser = require('body-parser');
+
+const exec = require('child_process').exec;
+//const spawn = require('child_process').spawn;
 
 const app = express();
 const script_path = '/Users/sijin.cao/Desktop/gitclone.sh';
 
+app.use(bodyParser.json());
+
 app.post('/build-rss', function(req, res) {
-	//console.log(req.body);
-	const child_process = spawn(script_path, {
-		shell: true
-	});
-	child_process.stdout.pipe(process.stdout);
-	child_process.on('exit', (code, signal)=> {
-		console.log(`exit code: ${code}`);
-		console.log(`signal: ${signal}`);
-		res.status(200).send();
-	});
-	//const child_process = exec(script_path, (error, stdout, stderr)=> {
-	//	console.log(`stdout ${stdout}`);
-	//	res.status(200).send();
-	//	if (error !== null) {
-	//		console.log(`error ${error}`);
-	//	}
+	console.log(req.body);
+
+	//child_process.spawn()
+	//const child_process = spawn(script_path, {
+	//	shell: true
 	//});
 	//child_process.stdout.pipe(process.stdout);
-});
+	//child_process.on('exit', (code, signal)=> {
+	//	console.log(`exit code: ${code}`);
+	//	console.log(`signal: ${signal}`);
+	//	res.status(200).send();
+	//});
 
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
+	//child_process.exec()
+	const repoUrl = req.body.repository.url;
+	var arr = repoUrl.split('/');
+	const projectPath = arr[arr.length-1].replace('.git', '');
+	const command = script_path + ' --repo ' + repoUrl + ' --project_path ' + projectPath;
+	const child_process = exec(command, (error)=> {
+		if (error !== null) {
+			console.log(`error ${error}`);
+			res.status(500).send(error);
+		} else {
+			res.status(200).send('shell script successfully executed');
+		}
 	});
+	child_process.stdout.pipe(process.stdout);
 });
 
 app.listen(3001);
