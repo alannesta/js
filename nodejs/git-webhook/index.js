13555e021d80c0bb7a76d4279ebc1876db356cfc
Alan Cao <alannesta@sina.com>
2016-03-17 11:10:55 -0400

    executing shell script, pipe stdout

--- nodejs/git-webhook/index.js
+++ nodejs/git-webhook/index.js
@@ -1,11 +1,30 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+//const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 
-var app = express();
+const app = express();
+const script_path = '/Users/sijin.cao/Desktop/gitclone.sh';
 
 app.post('/build-rss', function(req, res) {
-	console.log(req);
-	res.status(200).send();
+	//console.log(req.body);
+	const child_process = spawn(script_path, {
+		shell: true
+	});
+	child_process.stdout.pipe(process.stdout);
+	child_process.on('exit', (code, signal)=> {
+		console.log(`exit code: ${code}`);
+		console.log(`signal: ${signal}`);
+		res.status(200).send();
+	});
+	//const child_process = exec(script_path, (error, stdout, stderr)=> {
+	//	console.log(`stdout ${stdout}`);
+	//	res.status(200).send();
+	//	if (error !== null) {
+	//		console.log(`error ${error}`);
+	//	}
+	//});
+	//child_process.stdout.pipe(process.stdout);
 });
 
 app.use(function(err, req, res, next) {
