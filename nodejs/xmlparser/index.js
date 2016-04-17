87436074ec3bcb81f2dbea47e88a1734ab7d23a3
Alan Cao <alannesta@sina.com>
2016-04-16 22:37:20 -0400

    trending table scaffold

--- nodejs/xmlparser/index.js
+++ nodejs/xmlparser/index.js
@@ -5,6 +5,7 @@ var async = require('async');
 
 var crawlerService = require('./services/crawler-service');
 var dbService = require('./services/db-service');
+var miner = require('./services/mining-service');
 
 var urlPool = [
 	'http://www.91porn.com/v.php?category=hot&viewtype=basic&page=1',
@@ -13,14 +14,16 @@ var urlPool = [
 	'http://www.91porn.com/v.php?category=hot&viewtype=basic&page=4'
 ];
 
-async.eachSeries(urlPool, fetchVideo, function(err) {
-	if (err) {
-		console.log(err);
-		process.exit(err);
-	}
-	console.log('all done!');
-	process.exit(0);
-});
+//async.eachSeries(urlPool, fetchVideo, function(err) {
+//	if (err) {
+//		console.log(err);
+//		process.exit(err);
+//	}
+//	console.log('all done!');
+//	process.exit(0);
+//});
+
+miner.updateTrending();
 
 function fetchVideo(url, callback) {
 	crawlerService.crawl(url).then(function(videos){
--- nodejs/xmlparser/services/mining-service.js
+++ nodejs/xmlparser/services/mining-service.js
@@ -1,7 +1,61 @@
-//function trending(viewCount, time) {
-//	try {
-//		return (parseInt(viewCount, 10) / parseInt(time, 10)).toFixed(2);
-//	} catch (err) {
-//		console.log(err);
-//	}
+var connection = require('../utils/mysql-connector');
+
+var DataMiner = {
+	updateTrending: function() {
+		var query = `SELECT title, url, length, view_count, favourite, date_created, comment
+						FROM videos`;
+
+		var query2 = `INSERT INTO hot (title, url, length, trend, view_count, favourite, comment)
+						VALUES ? ON DUPLICATE KEY
+						UPDATE view_count=VALUES(view_count), favourite=VALUES(favourite), comment=VALUES(comment)`;
+
+		connection.query(query, function(err, results) {
+			//console.log(results);
+			if (err) {
+				throw err;
+			} else {
+				var payload = [];
+				results.forEach(function(result) {
+					//console.log('Trend: ---> ' + trend);
+					payload.push([result.title, result.url, result.length, trending(result.view_count, result.date_created), result.view_count, result.favourite, result.comment]);
+
+				});
+				connection.query(query2, [payload], function(err, results) {
+					if(err) {
+						console.log('update Trend Err: ' + err);
+					}else {
+						console.log(results);
+					}
+				})
+			}
+		});
+	}
+};
+
+function trending(viewCount, dateCreated) {
+
+	var now = Date.now();		//TODO: use time_updated rather than now
+	var created = new Date(dateCreated).getTime();
+	var time = (now - created) / 3600000;		//convert to hour
+
+	try {
+		return Math.floor(parseInt(viewCount, 10) / time);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+//function generateInsertValues(objects, keys) {
+//	var items = [];
+//	objects.forEach(function(object) {
+//		var entry = [];
+//		keys.forEach(function(key) {
+//			entry.push(object[key]);
+//		});
+//		items.push(entry);
+//	});
+//	return items;
 //}
+
+
+module.exports = DataMiner;
