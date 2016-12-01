aa91554d09c2e5c37a3c8f504b7918da6045c514
Alan Cao <alannesta@sina.com>
2016-12-01 08:59:53 -0500

    multipart/form-data upload

--- nodejs/quick_express/quick_server/routes/file-upload.js
+++ nodejs/quick_express/quick_server/routes/file-upload.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
+var multer = require('multer');
 //var rawParser = require('body-parser').raw();
+
+var upload = multer({ dest: '/Users/alancao/Desktop' });
+
 
 router.get('/', function(req, res) {
 	// without using the jade engine
@@ -14,14 +18,14 @@ router.get('/', function(req, res) {
 router.post('/upload', function(req, res) {
 
 	// #1 stream pipeline
-	var writable = fs.createWriteStream('/Users/alancao/Desktop/' + req.query.name);
-	req.pipe(writable);
-
-	req.on('end', function() {
-		writable.close();	// is this needed?
-		res.status(200).send('done');
-
-	});
+	//var writable = fs.createWriteStream('/Users/alancao/Desktop/' + req.query.name);
+	//req.pipe(writable);
+	//
+	//req.on('end', function() {
+	//	writable.close();	// is this needed?
+	//	res.status(200).send('done');
+	//
+	//});
 
 	// #2 keep all buffer/chunks in memory, write once
 
@@ -32,7 +36,6 @@ router.post('/upload', function(req, res) {
 	}).on('end', function() {
 		// chunks are already buffers, just need to concat
 		var buffer = Buffer.concat(body);
-		console.log(buffer);
 
 		fs.writeFile('/Users/alancao/Desktop/' + req.query.name, buffer, function(err){
 			if (err) {
@@ -53,5 +56,12 @@ router.post('/upload', function(req, res) {
 		console.log(err);
 	});
 });
+
+// using multer middleware to handle multipart/form-data
+router.post('/form-upload', upload.single('file'), function(req, res) {
+	console.log(req.file);
+	res.status(200).send('OK');
+});
+
 
 module.exports = router;
