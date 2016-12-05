37bccd3db264084d38a3b89978a7c444526642a7
Alan Cao <alannesta@sina.com>
2016-12-05 15:54:48 -0500

    upadated generator example, yield promise

--- ES6/src/features/generator.js
+++ ES6/src/features/generator.js
@@ -7,12 +7,30 @@ function* idMaker(){
 }
 
 function* generatorDemo() {
-	let a = function() {
+	// let a = function() {
+	// 	return 10;
+	// };
+	//
+	// let b = new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		resolve('promise resovled');
+	// 	}, 1500);
+	// });
+	console.log('2. [GEN] generator yield');
+	// let c = yield a;
+	// directly yield an expression
+	let c = yield function() {
 		return 10;
 	};
-
-	let c = yield a;
-	console.log('value received from caller: ', c);
+	console.log('5. [GEN] value received from caller: ', c);
+	console.log('6. [GEN] generator yield');
+	// let d = yield b;
+	let d = yield new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('promise resovled');
+		}, 1500);
+	});
+	console.log('8. [GEN] value received from caller: ', d);
 }
 
 // test in node
@@ -24,9 +42,19 @@ function* generatorDemo() {
 
 // in and out
 let iterator = generatorDemo();
+console.log('1. [CALLER] kick start generator');
 let fn = iterator.next().value;
-console.log('value received from generator: ', typeof fn);
-console.log('resume execution');
-iterator.next(fn.call());	// resume execution, pass in value
+console.log('3. [CALLER] calling next, value received from generator: ', typeof fn);
+console.log('4. [CALLER] resume execution, pass back function execution result');
+let promise = iterator.next(fn.call()).value;	// resume execution, pass in value, paused at next yield which yield a promise
+if (typeof promise.then === 'function') {
+	promise.then((val) => {
+		console.log('7. [CALLER] calling next, promise resolved, passback result');
+		let generatorEnd = iterator.next(val);
+		console.log('9. [CALLER] generator execution ended');
+		console.log(generatorEnd);
+	});
+}
+
 
 module.exports = idMaker;
