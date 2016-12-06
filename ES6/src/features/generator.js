6d1f713421c07c9150e75f862da8eb55fc8dbd9c
Alan Cao <alannesta@sina.com>
2016-12-06 08:12:43 -0500

    generator error handling

--- ES6/src/features/generator.js
+++ ES6/src/features/generator.js
@@ -33,7 +33,37 @@ function* generatorDemo() {
 	console.log('8. [GEN] value received from caller: ', d);
 }
 
-// test in node
+function* errorHandling() {
+	let errFn = function() {
+		return JSON.parse('2');
+		//return JSON.parse({name: "kaka"});
+	};
+
+	let asyncThunk = function() {
+		return function(callback) {
+			setTimeout(function() {
+				try {
+					callback(null, JSON.parse({name: "kaka"}));
+				} catch (err) {
+					callback(err);
+				}
+
+			},1500);
+		}
+	};
+
+	try {
+		yield 5;
+		yield errFn();
+		yield asyncThunk;
+		yield 'back to normal';
+	}catch(err) {
+		console.log('handle error: ', err);
+	}
+
+}
+
+// gasic generator
 //let iterator = idMaker();
 //console.log(iterator.next().value);
 //console.log(iterator.next().value);
@@ -41,20 +71,41 @@ function* generatorDemo() {
 //console.log(iterator.next().value);
 
 // in and out
-let iterator = generatorDemo();
-console.log('1. [CALLER] kick start generator');
-let fn = iterator.next().value;
-console.log('3. [CALLER] calling next, value received from generator: ', typeof fn);
-console.log('4. [CALLER] resume execution, pass back function execution result');
-let promise = iterator.next(fn.call()).value;	// resume execution, pass in value, paused at next yield which yield a promise
-if (typeof promise.then === 'function') {
-	promise.then((val) => {
-		console.log('7. [CALLER] calling next, promise resolved, passback result');
-		let generatorEnd = iterator.next(val);
-		console.log('9. [CALLER] generator execution ended');
-		console.log(generatorEnd);
-	});
-}
+//let iterator = generatorDemo();
+//console.log('1. [CALLER] kick start generator');
+//let fn = iterator.next().value;
+//console.log('3. [CALLER] calling next, value received from generator: ', typeof fn);
+//console.log('4. [CALLER] resume execution, pass back function execution result');
+//let promise = iterator.next(fn.call()).value;	// resume execution, pass in value, paused at next yield which yield a promise
+//if (typeof promise.then === 'function') {
+//	promise.then((val) => {
+//		console.log('7. [CALLER] calling next, promise resolved, passback result');
+//		let generatorEnd = iterator.next(val);
+//		console.log('9. [CALLER] generator execution ended');
+//		console.log(generatorEnd);
+//	});
+//}
 
+// error handling
+let iterator = errorHandling();
+console.log(iterator.next());
+console.log(iterator.next());
+let asyncThunk = iterator.next().value;
+console.log(asyncThunk);
+new Promise((resolve, reject) => {
+	asyncThunk().call(null, (err, result) => {
+		if(err) {
+			reject(err);
+		}
+		resolve(result);
+	});
+}).then((result) => {
+	console.log(iterator.next(result));
+}).catch((err) => {
+	console.log('error handled, throw back?: ', err);
+	//iterator.throw(err);	// iterator will continue execution if err is not throw back
+	console.log(iterator.next());
+	console.log(iterator.next());
+});
 
 module.exports = idMaker;
