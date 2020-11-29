3bef3a3abe23d522eb4151d4dd7bfff38fa0e5a9
Alan Cao <alannesta@sina.com>
2020-11-29 16:44:12 -0500

    explore promise, timer and node process

--- nodejs/basic_api/quick-promise.js
+++ nodejs/basic_api/quick-promise.js
@@ -1,15 +1,34 @@
 // nodejs does not "hang" on unresolved promise
-async function main() {
-    await new Promise((resolve, reject) => {
-        console.log(42);
-        // setTimeout(() => {
-        //     resolve('lol')
-        // }, 2000)
-    });
-    console.log(`
-        This will never be executed is this expected?
-        Also node exits as everything is ok with exit code 0...
-      `);
+
+// node v12.13.0
+// async function main() {
+//     await new Promise((resolve, reject) => {
+//         console.log('promise that will not be unresolved');
+//         // adding a pending timer will keep node process "alive"
+//         // setTimeout(() => {
+//         //     resolve('keep the node process ALIVE!!!')
+//         // }, 2000)
+//     });
+//     console.log(`
+//         This will never be executed is this expected?
+//         Also node exits as everything is ok with exit code 0...
+//       `);
+// }
+
+// node v6.9.4
+function main() {
+    let kaka = new Promise((resolve, reject) => {
+        console.log('promise that will not be unresolved without a timer');
+
+        // adding a timer
+        setTimeout(() => {
+            resolve('keep the node process ALIVE!!!');
+        }, 2000);
+    }).then(() => {
+        // this will not be called without a settimeout
+        console.log('promise resolve!!');
+    })
 }
 
 main();
+
